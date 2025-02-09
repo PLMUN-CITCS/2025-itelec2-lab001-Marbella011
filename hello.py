e5286e8312ef7f5576cdb1e36af8b7341c9f60e8
Bernard Marbella <130128112+Marbella011@users.noreply.github.com>
2025-02-09 20:17:14 +0800

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,6 @@
 print("Hello world!")
+
+print("Hello world!")
+print("Name: [Marbella Bernard]")
+print("Course: [Bachelor of Science in Information Technology]")
+print("Interests in programming: [I'm interested to learn everything.]")
